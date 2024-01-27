13ef41f55d66ea0ea29b675f755e31b4edadf0db
xtombrock <xtombrock1989@gmail.com>
2024-01-27 21:32:54 +0000

    init

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -21,23 +21,7 @@ function HomePage() {
 
   return (
     <div className='grid main__bg font-poppins h-screen grid-rows-reg overflow-hidden max-h-screen'>
-      <Navbar />
-      {/* Main */}
-      <main className='grid h-full p-1 items-center justify-center'>
-        <section>
-          <div className='grid text-center outline outline-2 outline-black rounded-xl bg-yellow-400 px-6 py-8'>
-            <article className=''>
-              <div>
-                <h2 className='text-xl font-semibold'>Welcome To</h2>
-                <h1 className='text-3xl font-poppins font-bold'>
-                  Test Project
-                </h1>
-              </div>
-
-            </article>
-          </div>
-        </section>
-      </main>
+     <main>he</main>
     </div>
   );
 }
